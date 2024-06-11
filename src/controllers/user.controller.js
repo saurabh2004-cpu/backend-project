7cb8da6cd5f8fa9d45c77be74632ab0d4dea1d23
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-11 11:28:23 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 // import { response } from "express"
 import { asyncHandler } from "../utils/asyncHandler.js"
-import ApiResponse from "../utils/apiResponse.js"
+import {ApiResponse} from "../utils/apiResponse.js"
 import { ApiError } from "../utils/apiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
