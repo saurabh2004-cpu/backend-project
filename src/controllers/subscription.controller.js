ee21f5dfd55876c5ce803059c981a1e66bd96a07
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-11 11:13:54 +0530

    commit

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose"
 import {User} from "../models/user.model.js"
 import { Subscription } from "../models/subscription.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+// import {ApiError} from "../utils/ApiError.js"
+// import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponse from "../utils/apiResponse.js"
+import { ApiError } from "../utils/apiError.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {Video} from "../models/video.model.js"
+import { ApiError } from "../utils/apiError.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params;
