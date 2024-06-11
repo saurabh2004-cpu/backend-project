98712bed11fb89d6c6e66b505a2cbb26b8b715f3
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-11 11:22:28 +0530

    c

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,6 +1,6 @@
 import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import ApiResponse from "../utils/apiResponse.js"
+import { ApiError } from "../utils/apiError.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
