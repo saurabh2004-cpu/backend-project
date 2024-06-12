13e4712c1780eac4c2ae2b97be37e33b673f5010
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-12 15:42:54 +0530

    -

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-const allowedOrigins = 'https://youtubeclone-axpb.vercel.app';
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: process.env.CORS_ORIGIN,    //indicates that from whre the request is accept from the frontend ex-vercel,netlify etc
     credentials: true,
-  }));
+}))
 
 app.use(express.json({ limit: "16kb" }))                            //form data-accept the 16kb of json
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))      //url data-allow nested objects,and the limit is 16kb
