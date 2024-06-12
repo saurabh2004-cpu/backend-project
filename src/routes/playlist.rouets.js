2caa8e06b96d0a79abdbe372f6add94b1c99307a
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-12 11:25:09 +0530

    -

--- src/routes/playlist.rouets.js
+++ src/routes/playlist.rouets.js
@@ -17,8 +17,8 @@ const playListRouter=Router()
 playListRouter.route("/create-playlist").post(verifyJwt,createPlaylist)
 
 
-playListRouter.route("/get-all-playlists/:channelId").get(verifyJwt,getUserPlaylists)
-playListRouter.route("/get-playlist/:playlistId").get(verifyJwt,getPlaylistById)
+playListRouter.route("/get-all-playlists/:channelId").get(getUserPlaylists)
+playListRouter.route("/get-playlist/:playlistId").get(getPlaylistById)
 playListRouter.route("/add-video-to-playlist/:playlistId/video/:videoId").post(verifyJwt,addVideoToPlaylist)
 playListRouter.route("/remove-video/:playlistId/videos/:videoId").post(verifyJwt,removeVideoFromPlaylist)
 playListRouter.route("/delete-playlist/:playlistId").post(verifyJwt,deletePlaylist)
