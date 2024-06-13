1c745d52be0ef27b5dfb1813940bd6ef8e920173
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-13 09:44:57 +0530

    -

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,7 +47,7 @@ router.route("/change-password").post(verifyJwt,changeCurrentPassword)
 router.route("/get-current-user").get(verifyJwt, getCurrentUser)
 router.route("/update-account-details").patch(verifyJwt,updateAccountDetails)
 
-router.route("/update-avatar").post(verifyJwt, upload.single("avatar"),updateUserAvatar)
+router.route("/update-avatar").patch(verifyJwt, upload.single("avatar"),updateUserAvatar)
 router.route("/update-cover-image").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/get-channel-profile/:channelId").get(getUserChannelProfile)
